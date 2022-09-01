8f20da13413c6515c9e21072bab40243ed6b8929
NishChal370 <nishchal.370@gmail.com>
2022-09-01 22:14:42 +0545

    Design page heading

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,19 +1,30 @@
-import { Layout, PageHeader } from 'antd';
+import { Layout, PageHeader, Typography } from 'antd';
 import { ContactDetails } from '../component';
 
 const { Content } = Layout;
+const { Title } = Typography;
 
 function Home() {
       
       return (
             <Layout className='home__container  bg-semi-white flex flex-col justify-center gap-10 w-[98%] self-center 
+                              sm:gap-14
                               xl:w-[80rem]
                               2xl:w-[94rem]'
             >
-                  <PageHeader
-                        className="p-0 m-0"
-                        title="Title"
-                  />
+                  <Content className='flex  flex-col self-center justify-center'>
+
+                  
+                        <img
+                              className='h-30 self-center
+                                    sm:w-[30rem] '
+                              src='https://selectra.co.uk/sites/selectra.co.uk/files/images/sky_contact_number_banner.png' alt="" 
+                        />
+                        
+                        <Title className="text-6xl p-0 m-0 self-center flex gap-2 text-center">
+                              <span className='text-blue-600'>Contact Detail</span>
+                        </Title>
+                  </Content>
 
                   <Content className='home-body__container  grid grid-cols-1 gap-10
                                     sm:grid-cols-2 sm:gap-y-14
